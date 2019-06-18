2cf617b8b2c286397350218238cb3ba52c2d41eb
Jiab77 <jonathan.barda@gmail.com>
2019-06-18 05:04:46 +0200

    Fixed array issue but having new ones to fix now

--- json2html.py
+++ json2html.py
@@ -28,26 +28,32 @@ import array
 import pprint
 
 class json2html:
-	SELF_CLOSING_TAGS = ['meta', 'img', 'br', 'hr']
+	SELF_CLOSING_TAGS = ('meta', 'img', 'br', 'hr')
 	DEFAULT_MARKER = '${}'
-	MARKERS = ['{{}}', '${}']
-	DEBUG = false
-	DATA = null
+	MARKERS = ('{{}}', '${}')
+	DEBUG = False
+	DATA = None
 
 	iterations = 0
-	buffer = {}
+	buffer = []
 	cli = False
 	html = ''
-	props = {}
+	props = []
 
 	is_array = lambda var: isinstance(var, (list, tuple))
 
 	@staticmethod
 	def _convert(decoded_tags, debug):
+		nonlocal iterations
+		nonlocal buffer
+		nonlocal html
+		nonlocal props
+		nonlocal SELF_CLOSING_TAGS
+		
 		# Init internal counter
-		self.iterations += 1
+		iterations += 1
 		if (debug == True):
-			print('Iteration:', self.iterations + '\n')
+			print('Iteration:', iterations + '\n')
 		
 		# Parse given tags
 		for (key, value) in decoded_tags.items():
@@ -56,29 +62,29 @@ class json2html:
 					print('Converted [{}] to: <{}>\n', value, value)
 					print('is self closing tag:', 'true' if value in SELF_CLOSING_TAGS else 'false')
 
-				self.buffer[] = ['iteration' = self.iterations, 'tag' = value]
+				buffer.append({'iteration': iterations, 'tag': value})
 
 			elif (key == 'alt' or key == 'class' or key == 'id' or key == 'src' or key == 'href' or key == 'target' or key == 'name' or key == 'action' or key == 'method' or key == 'style'):
 				if (debug == True):
 					print('Converted to: {} = {}\n', key, value)
 				
-				self.props[] = ['iteration' = self.iterations, 'attribute' = key + '="' + value + '"']
+				props.append({'iteration': iterations, 'attribute': key + '="' + value + '"'})
 			
 			elif (key == 'child' or key == 'children' or key == 'html'):
 				if (is_array(value)):
 					for html_tags in value.values():
-						self._convert(html_tags, debug)
+						json2html._convert(html_tags, debug)
 				else:
 					if (debug == True):
 						print('Converted to: innerHTML="{}"\n', value)
 					
-					self.props[] = ['iteration' = self.iterations, 'content' = value]
+					props.append({'iteration': iterations, 'content': value})
 			
 			elif (key == 'text'):
 				if (debug == True):
 					print('Converted to: innerText="{}"\n', value)
 				
-				self.props[] = ['iteration' = self.iterations, 'content' = value]
+				props.append({'iteration': iterations, 'content': value})
 			
 			else:
 				print('Unsupported tag given. Got: "{}"\n', key)
@@ -86,29 +92,33 @@ class json2html:
 	
 	@staticmethod
 	def _merge():
+		nonlocal buffer
+		nonlocal props
+		nonlocal html
+
 		# Loop on tags
 		i = 0
-		while i <= len(self.buffer)-1:
+		while i <= len(buffer)-1:
 			# Open tag
-			self.html += '<' + self.buffer[i]['tag']
+			html += '<' + buffer[i]['tag']
 
 			# Adding attributes
 			j = 0
-			while j <= len(self.props)-1:
-				if (self.props[j]['iteration'] == self.props[i]['iteration']):
-					self.html += ' ' + self.props[j]['attribute'] if 'attribute' in self.props[j] else ''
+			while j <= len(props)-1:
+				if (props[j]['iteration'] == props[i]['iteration']):
+					html += ' ' + props[j]['attribute'] if 'attribute' in props[j] else ''
 				
 				# Increment counter
 				j += 1
 			
 			# Closing open tag
-			self.html += '>'
+			html += '>'
 
 			# Adding content
 			j = 0
-			while j <= len(self.props)-1:
-				if (self.props[j]['iteration'] == self.props[i]['iteration']):
-					self.html += self.props[j]['content'] if 'content' in self.props[j] else ''
+			while j <= len(props)-1:
+				if (props[j]['iteration'] == props[i]['iteration']):
+					html += props[j]['content'] if 'content' in props[j] else ''
 				
 				# Increment counter
 				j += 1
@@ -117,28 +127,31 @@ class json2html:
 			i += 1
 
 		# Close tag
-		i = len(self.buffer)-1
+		i = len(buffer)-1
 		while i >= 0:
-			if (in_array(self.buffer[i]['tag'], SELF_CLOSING_TAGS) == False):
-				self.html += '</' + self.buffer[i]['tag'] + '>'
+			if (in_array(buffer[i]['tag'], SELF_CLOSING_TAGS) == False):
+				html += '</' + buffer[i]['tag'] + '>'
 			
 			# Deincrement counter
 			i -= 1
 
 	@staticmethod
 	def _replace(html, data, marker, debug):
+		nonlocal MARKERS
+
 		# Validate given marker
 		if (in_array(marker, MARKERS)):
 			if (marker == '{{}}'):
-				re = r'/({{)(\w*)(}})(\w*)/m'
+				regex = r'/({{)(\w*)(}})(\w*)/m'
 			elif (marker == '${}'):
-				re = r'/(\${)(\w*)(})(\w*)/m'
+				regex = r'/(\${)(\w*)(})(\w*)/m'
 		else:
 			print('Unsupported marker given.\n')
 			return False
 
 		# Apply changes
-		if (matches = re.finditer(re, html, 0)):
+		matches = re.finditer(regex, html, 0)
+		if (len(matches) > 0):
 			count = len(matches)
 
 			if (debug == True):
@@ -148,7 +161,7 @@ class json2html:
 				index = 0
 				for match in matches:
 					if (match[2] != '' and isinstance(match[2], str)):
-						self.html = self.html.replace(match[0], data[index]->{match[2]})
+						html = html.replace(match[0], data[index][match[2]])
 						index += 1
 		else:
 			print('Failed to process data.\n')
@@ -158,6 +171,10 @@ class json2html:
 	def transform(tags, data = DATA, debug = DEBUG, marker = DEFAULT_MARKER):
 		if (tags != ''):
 			if (isinstance(tags, str)):
+				nonlocal buffer
+				nonlocal props
+				nonlocal html
+
 				# Decode stuff
 				tags = tags.decode().encode('UTF-8')
 				decoded_tags = json.loads(tags)
@@ -172,34 +189,34 @@ class json2html:
 					# Convert tags
 					if (debug == True):
 						print('\nDecoded:\n')
-						print dir(decoded_tags)
-						print dir(decoded_data)
+						print(dir(decoded_tags))
+						print(dir(decoded_data))
 						print('\nConverting...\n')
 					
-					self._convert(decoded_tags, debug)
+					json2html._convert(decoded_tags, debug)
 
 					# Merge buffer and props
 					if (debug == True):
 						print('\nBuffer:\n')
-						pprint.pprint(self.buffer)
+						pprint.pprint(buffer)
 						print('\nProps:\n')
-						pprint.pprint(self.props)
+						pprint.pprint(props)
 						print('\nMergin...\n')
 					
-					self._merge()
+					json2html._merge()
 
 					# Parse given data
 					if (decoded_data != None):
 						if (debug == True):
 							print('Adding data...\n')
 						
-						self._replace(self.html, decoded_data, marker, debug)
+						json2html._replace(html, decoded_data, marker, debug)
 					
 					# Output result
-					return self.html
+					return html
 				else:
-					print dir(decoded_tags)
-					print dir(data)
+					print(dir(decoded_tags))
+					print(dir(data))
 					return False
 			else:
 				print('Tags must be a string. {} given.', type(tags).__name__.capitalize())
