1fce8da9ee1c87798ff6354b9af01d9f76fbf87a
Jiab77 <jonathan.barda@gmail.com>
2019-06-19 02:37:00 +0200

    Added test versions

---
+++ json2html.test.php
@@ -0,0 +1,313 @@
+<?php
+/*
+MIT License
+
+Copyright (c) 2019 Jonathan Barda
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+class json2html
+{
+	const SELF_CLOSING_TAGS = ['meta', 'img', 'br', 'hr'];
+	const DEFAULT_MARKER = '${}';
+	const MARKERS = ['{{}}', '${}'];
+	const DEBUG = false;
+	const DATA = null;
+
+	private static $iterations = 0;
+	private static $buffer = [];
+	private static $cli = false;
+	private static $html = '';
+	private static $props = [];
+
+	private static function _check_extensions()
+	{
+		if (!extension_loaded('mbstring')) {
+			echo 'Extension mbstring not loaded.';
+			return false;
+		}
+		if (!extension_loaded('json')) {
+			echo 'Extension json not loaded.';
+			return false;
+		}
+	}
+
+	private static function _check_sapi()
+	{
+		if (substr(PHP_SAPI, 0, 3) !== 'cgi') {
+			self::$cli = true;
+		}
+	}
+	
+	private static function _convert($decoded_tags, $debug)
+	{
+		// Init internal counter
+		self::$iterations++;
+		if ($debug === true) {
+			echo 'Iteration: ' . self::$iterations . PHP_EOL;
+		}
+
+		// Parse given tags
+		foreach ($decoded_tags as $key => $value) {
+			switch ($key) {
+				case 'tag':
+				case '<>':
+					if ($debug === true) {
+						echo 'Converted [' . $value . '] to: <' . $value . '>' . PHP_EOL;
+						echo 'is self closing tags: ' . (in_array($value, self::SELF_CLOSING_TAGS) ? 'true' : 'false') . PHP_EOL;
+					}
+
+					self::$buffer[] = ['iteration' => self::$iterations, 'tag' => $value];
+					break;
+
+				case 'alt':
+				case 'class':
+				case 'id':
+				case 'src':
+				case 'href':
+				case 'target':
+				case 'name':
+				case 'action':
+				case 'method':
+				case 'style':
+					if ($debug === true) {
+						echo 'Converted to: ' . $key . '="' . $value . '"' . PHP_EOL;
+					}
+
+					self::$props[] = ['iteration' => self::$iterations, 'attribute' => $key . '="' . $value . '"'];
+					break;
+
+				case 'child':
+				case 'children':
+				case 'html':
+					if (is_array($value)) {
+						foreach ($value as $html_tags) {
+							self::_convert($html_tags, $debug);
+						}
+					}
+					else {
+						if ($debug === true) {
+							echo 'Converted to: innerHTML="' . htmlentities(strip_tags($value)) . '"' . PHP_EOL;
+						}
+
+						self::$props[] = ['iteration' => self::$iterations, 'content' => htmlentities(strip_tags($value))];
+					}
+					break;
+
+				case 'text':
+					if ($debug === true) {
+						echo 'Converted to: innerText="' . html_entity_decode(strip_tags($value)) . '"' . PHP_EOL;
+					}
+
+					self::$props[] = ['iteration' => self::$iterations, 'content' => html_entity_decode(strip_tags($value))];
+					break;
+				
+				default:
+					echo 'Unsupported tag given. Got: "' . $key . '"' . PHP_EOL;
+					return false;
+					break;
+			}
+		}
+	}
+
+	private static function _merge()
+	{
+		// Loop on tags
+		for ($i = 0; $i <= count(self::$buffer)-1; $i++) {
+			// Open tag
+			self::$html .= '<' . self::$buffer[$i]['tag'];
+
+			// Adding attributes
+			for ($j = 0; $j <= count(self::$props)-1; $j++) {
+				if (self::$props[$j]['iteration'] === self::$buffer[$i]['iteration']) {
+					self::$html .= (array_key_exists('attribute', self::$props[$j]) !== false ? ' ' . self::$props[$j]['attribute'] : '');
+				}
+			}
+
+			// Closing open tag
+			self::$html .= '>';
+
+			// Adding content
+			for ($j = 0; $j <= count(self::$props)-1; $j++) {
+				if (self::$props[$j]['iteration'] === self::$buffer[$i]['iteration']) {
+					self::$html .= (array_key_exists('content', self::$props[$j]) !== false ? self::$props[$j]['content'] : '');
+				}
+			}
+		}
+
+		// Close tag
+		for ($i = count(self::$buffer)-1; $i >= 0; $i--) {
+			if (!in_array(self::$buffer[$i]['tag'], self::SELF_CLOSING_TAGS)) {
+				self::$html .= '</' . self::$buffer[$i]['tag'] . '>';
+			}
+		}
+	}
+
+	private static function _replace($html, $data, $marker, $debug)
+	{
+		// Validate given marker
+		if (in_array($marker, self::MARKERS)) {
+			switch ($marker) {
+				case '{{}}':
+					$re = '/({{)(\w*)(}})(\w*)/m';
+					break;
+				case '${}':
+					$re = '/(\${)(\w*)(})(\w*)/m';
+					break;
+			}
+		}
+		else {
+			echo 'Unsupported marker given.' . PHP_EOL;
+			return false;
+		}
+		
+		// Apply changes
+		if (preg_match_all($re, $html, $matches, PREG_SET_ORDER, 0) !== false) {
+			$count = count($matches);
+				
+			if ($debug === true) {
+				// Print the entire match result
+				var_dump($matches);
+			}
+	
+			if ($count === count($data)) {
+				$index = 0;
+				foreach ($matches as $match) {
+					if (!empty($match[2]) && is_string($match[2])) {
+						self::$html = str_replace($match[0], $data[$index]->{$match[2]}, self::$html);
+						$index++;
+					}
+				}
+			}
+		}
+		else {
+			echo 'Failed to process data.' . PHP_EOL;
+			return false;
+		}
+	}
+
+	public static function transform($tags, $data = self::DATA, $debug = self::DEBUG, $marker = self::DEFAULT_MARKER)
+	{
+		if (!empty($tags)) {
+			if (is_string($tags)) {
+				// Check internals
+				self::_check_extensions();
+				self::_check_sapi();
+
+				// Output internal debug on CLI
+				if (self::$cli === true) {
+					echo 'Received:' . PHP_EOL;
+					var_dump($tags, $data);
+				}
+
+				// Decode stuff
+				$tags = mb_convert_encoding($tags, 'UTF-8');
+				$decoded_tags = json_decode($tags);
+				if (!is_null($data)) {
+					$data = mb_convert_encoding($data, 'UTF-8');
+					$decoded_data = json_decode($data);
+				}
+				else {
+					$decoded_data = $data;
+				}
+
+				// Check decoding result
+				if ($decoded_tags) {
+					// Convert tags
+					if ($debug === true) {
+						echo PHP_EOL . 'Decoded:' . PHP_EOL;
+						var_dump($decoded_tags, $decoded_data);
+						echo PHP_EOL . 'Converting...' . PHP_EOL;
+					}
+					self::_convert($decoded_tags, $debug);
+
+					// Merge buffer and props
+					if ($debug === true) {
+						echo PHP_EOL . 'Buffer:' . PHP_EOL;
+						print_r(self::$buffer);
+						echo PHP_EOL . 'Props:' . PHP_EOL;
+						print_r(self::$props);
+						echo PHP_EOL . 'Merging...' . PHP_EOL;
+					}
+					self::_merge();
+
+					// Parse given data
+					if (!is_null($decoded_data)) {
+						if ($debug === true) {
+							echo 'Adding data...' . PHP_EOL;
+						}
+						self::_replace(self::$html, $decoded_data, $marker, $debug);
+					}
+
+					// Output internal debug on CLI
+					if (self::$cli === true) {
+						echo PHP_EOL . 'Converted:' . PHP_EOL;
+						var_dump(self::$html);
+					}
+
+					// Output result
+					return self::$html;
+				}
+
+				// Return error
+				else {
+					echo 'Failed to parse tags/data.';
+					if ($debug === true) {
+						var_dump($decoded_tags, json_last_error(), json_last_error_msg(), $data);
+					}
+					return false;
+				}
+			}
+			else {
+				echo 'Tags must be a string. ' . ucfirst(gettype($tags)) . ' given.' . PHP_EOL;
+				return false;
+			}
+		}
+		else {
+			echo 'Empty tags given.' . PHP_EOL;
+			return false;
+		}
+	}
+}
+
+// Test payload
+// fail result
+// $payload = ['x', 'y'];
+// $payload = new stdClass;
+
+// pass result
+// $payload = '{"<>":"div","class":"card", "id":"' . uniqid() . '","html":[{"<>":"img", "src":"https://picsum.photos/id/' . mt_rand(0, 999) . '/400?random=' . uniqid() . '","alt":"this is our logo"},{"<>":"p","text":"Hi {{name}}! Welcome to json2html!"}]}';
+// $payload = '{"<>":"div","class":"{{class}}", "id":"' . uniqid() . '","html":[{"<>":"img", "src":"https://picsum.photos/id/' . mt_rand(0, 999) . '/400?random=' . uniqid() . '","alt":"this is our logo"},{"<>":"p","text":"Hi {{name}}! Welcome to json2html!"}]}';
+// $payload = '{"<>":"div","class":"${class}", "id":"' . uniqid() . '","html":[{"<>":"img", "src":"https://picsum.photos/id/' . mt_rand(0, 999) . '/400?random=' . uniqid() . '","alt":"this is our logo"},{"<>":"p","text":"Hi ${name}! Welcome to json2html!"}]}';
+$payload = '{"<>":"div","class":"${class}", "id":"${id}","html":[{"<>":"img", "src":"${src}","alt":"${alt}"},{"<>":"p","text":"Hi ${name}! Welcome to json2html!"}]}';
+
+// Test data
+// fail result
+// $data = '';
+// $data = '[]';
+// $data = '[{"id":"' . uniqid() . '"}, {"class":"card"}, {"src":"https://picsum.photos/id/' . mt_rand(0, 999) . '/400?random=' . uniqid() . '"}, {"alt":"this is our logo"}, {"name":"Jo"}]';
+
+// pass result
+// $data = '[{"name":"Jo"}]';
+// $data = '[{"class":"card"}, {"name":"Jo"}]';
+$data = '[{"class":"card"}, {"id":"' . uniqid() . '"}, {"src":"https://picsum.photos/id/' . mt_rand(0, 999) . '/400?random=' . uniqid() . '"}, {"alt":"this is our logo"}, {"name":"Jo"}]';
+
+// json2html::transform($payload, $data, true);
+json2html::transform($payload, $data);
---
+++ json2html.test.py
@@ -0,0 +1,350 @@
+"""
+MIT License
+
+Copyright (c) 2019 Jonathan Barda
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+import json
+import re
+import array
+import pprint
+import os
+import psutil
+
+class json2html:
+	SELF_CLOSING_TAGS = ('meta', 'img', 'br', 'hr')
+	DEFAULT_MARKER = '${}'
+	MARKERS = ('{{}}', '${}')
+	DEBUG = False
+	DATA = None
+	DISPLAY_PROPERTIES = False
+
+	iterations = 0
+	childs = 0
+	buffer = []
+	cli = False
+	html = ''
+	props = []
+
+	@staticmethod
+	def in_array(needle, haystack):
+		if (needle in haystack):
+			return True
+		return False
+	
+	@staticmethod
+	def is_array(needle):
+		return isinstance(needle, (list, tuple))
+
+	@staticmethod
+	def is_string(needle):
+		return isinstance(needle, str)
+
+	@staticmethod
+	def contains(needle, string):
+		if (string.find(needle) == -1):
+			return False
+		return True
+
+	@staticmethod
+	def display_banner():
+		os.system('clear')
+		print("""
+************
+* CLI Mode *
+************
+""")
+
+	@classmethod
+	def invoked_from_bash_cmdline(cls):
+		# print('Parent:', psutil.Process(os.getpid()).parent().name())
+		if (psutil.Process(os.getpid()).parent().name() == "bash"):
+			cls.cli = True
+			return True
+		return False
+	
+	@classmethod
+	def invoked_as_run_in_terminal(cls):
+		# print('Parent:', psutil.Process(os.getpid()).parent().name())
+		if (cls.contains('terminal', psutil.Process(os.getpid()).parent().name()) == True):
+			cls.cli = True
+			return True
+		return False
+	
+	@classmethod
+	def _convert(cls, decoded_tags, debug):
+		# Init internal counter
+		cls.iterations += 1
+		if (debug == True):
+			print('\n* Iteration:', cls.iterations)
+		
+		# Parse given tags
+		for (key, value) in decoded_tags.items():
+			if (key == 'tag' or key == '<>'):
+				cls.buffer.append({'iteration': cls.iterations, 'tag': value})
+				if (debug == True):
+					print('Converted [' + value + '] to: <' + value + '>')
+					print('is self closing tag:', 'true' if value in cls.SELF_CLOSING_TAGS else 'false')
+
+			elif (key == 'alt' or key == 'class' or key == 'id' or key == 'src' or key == 'href' or key == 'target' or key == 'name' or key == 'action' or key == 'method' or key == 'style'):
+				cls.props.append({'iteration': cls.iterations, 'attribute': key + '="' + value + '"'})
+				if (debug == True):
+					print('Converted to: ' + key + ' = "' + value + '"')
+			
+			elif (key == 'child' or key == 'children' or key == 'html'):
+				if (cls.is_array(value)):
+					for html_tags in value:
+						cls._convert(html_tags, debug)
+				else:
+					cls.props.append({'iteration': cls.iterations, 'content': value})
+					if (debug == True):
+						print('Converted to: innerHTML="' + value + '"')
+			
+			elif (key == 'text'):
+				cls.props.append({'iteration': cls.iterations, 'content': value})
+				if (debug == True):
+					print('Converted to: innerText="' + value + '"')
+				
+			else:
+				print('Unsupported tag given. Got: "' + key + '"\n')
+				return False
+	
+	@classmethod
+	def _merge(cls, debug):
+		# Loop on tags
+		i = 0
+		while i <= len(cls.buffer)-1:
+			# Debug pass
+			if (debug == True):
+				print('Pass:', i)
+
+			# Open tag
+			cls.html += '<' + cls.buffer[i]['tag']
+
+			# Reading props
+			if (debug == True):
+				print('Props:', cls.props)
+
+			# Adding attributes
+			for j in range(0, len(cls.props)):
+				if (cls.props[j]['iteration'] == cls.props[i]['iteration']):
+					# print('J:A:', cls.props[j]['iteration'], '==', 'I:A:', cls.props[i]['iteration'])
+					cls.html += ' ' + cls.props[j]['attribute'] if 'attribute' in cls.props[j] else ''
+					
+			# Closing open tag
+			cls.html += '>'
+
+			# Adding content
+			for k in range(0, len(cls.props)):
+				if (cls.props[k]['iteration'] == cls.props[i]['iteration']):
+					# print('K:C:', cls.props[k]['iteration'], '==', 'I:C:', cls.props[i]['iteration'])
+					cls.html += cls.props[k]['content'] if 'content' in cls.props[k] else ''
+			
+			# Remove consumed props[i] (passed around 3 hours on that crap!!)
+			del cls.props[i]
+
+			# Increment tag open counter
+			i += 1
+
+		# Close tag
+		i = len(cls.buffer)-1
+		while i >= 0:
+		# for i in range(0, len(cls.buffer)):
+			if (cls.in_array(cls.buffer[i]['tag'], cls.SELF_CLOSING_TAGS) == False):
+				cls.html += '</' + cls.buffer[i]['tag'] + '>'
+			
+			# Deincrement tag close counter
+			i -= 1
+
+	@classmethod
+	def _replace(cls, html, data, marker, debug):
+		# Additional debug infos
+		if (debug == True):
+			print('Data:', data)
+			print('Used marker:', marker)
+
+		# Validate given marker
+		if (cls.in_array(marker, cls.MARKERS)):
+			if (marker == '{{}}'):
+				regex = r"({{)(\w*)(}})(\w*)"
+			elif (marker == '${}'):
+				regex = r"(\${)(\w*)(})(\w*)"
+		else:
+			print('Unsupported marker given.\n')
+			return False
+
+		# Apply changes
+		matches = re.finditer(regex, html, re.MULTILINE)
+		if (debug == True):
+			print('Used regex:', regex)
+			print('Search in:', html)
+			print('Results:', matches)
+
+		if (matches):
+			## Loop on matches
+			index = 0
+			for matchNum, match in enumerate(matches, start=1):
+				if (debug == True):
+					print ("Match {matchNum} was found at {start}-{end}: {match}".format(matchNum = matchNum, start = match.start(), end = match.end(), match = match.group()))
+				cls.html = cls.html.replace(match.group(), data[index][match.group(2)])
+
+				## Increment data counter
+				index += 1
+			
+				""" for groupNum in range(0, len(match.groups())):
+					groupNum = groupNum + 1
+					print ("Group {groupNum} found at {start}-{end}: {group}".format(groupNum = groupNum, start = match.start(groupNum), end = match.end(groupNum), group = match.group(groupNum))) """
+		else:
+			print('Failed to process data.\n')
+			return False
+	
+	@classmethod
+	def transform(cls, tags, data = None, debug = None, marker = None):
+		# Run some internal methods
+		cls.invoked_from_bash_cmdline()
+		cls.invoked_as_run_in_terminal()
+
+		# Display banner when on CLI mode
+		if (cls.cli == True):
+			cls.display_banner()
+
+		# Save state
+		encoded_tags = tags
+		encoded_data = data
+
+		# Assign default props
+		if (data == None):
+			data = cls.DATA
+		
+		if (debug == None):
+			debug = cls.DEBUG
+		
+		if (marker == None):
+			marker = cls.DEFAULT_MARKER
+
+		# Display some internal debugging info
+		if (cls.cli == True):
+			if (debug == True):
+				if (cls.DISPLAY_PROPERTIES == True):
+					print('* Received:\n', encoded_tags, '\n\n ==> Properties:\n', dir(encoded_tags))
+				else:
+					print('* Received:\n', encoded_tags)
+			else:
+				print('Received:\n', encoded_tags)
+
+		# Check if you we got tags to process
+		if (encoded_tags != ''):
+			# Check given tags type
+			if (cls.is_string(encoded_tags)):
+				# Decode tags
+				decoded_tags = json.loads(encoded_tags)
+
+				# Decode data
+				if (encoded_data != None):
+					decoded_data = json.loads(encoded_data)
+				else:
+					decoded_data = encoded_data
+				
+				# Check decoding result
+				if (decoded_tags):
+					if (debug == True):
+						if (cls.DISPLAY_PROPERTIES == True):
+							print('\n* Decoded tags:\n', decoded_tags, '\n\n ==> Properties:\n', dir(decoded_tags))
+							print('\n* Decoded data:\n', decoded_data, '\n\n ==> Properties:\n', dir(decoded_data))
+						else:
+							print('\n* Decoded tags:\n', decoded_tags)
+							print('\n* Decoded data:\n', decoded_data)
+						print('\nConverting...')
+					
+					# Convert tags
+					cls._convert(decoded_tags, debug)
+
+					# Display memory info
+					if (debug == True):
+						print('\n* Buffer:', len(cls.buffer), '\n')
+						pprint.pprint(cls.buffer)
+						print('\n* Props:', len(cls.props), '\n')
+						pprint.pprint(cls.props)
+						print('\nMerging...\n')
+					
+					# Merge buffer and props
+					cls._merge(debug)
+
+					# Parse given data
+					if (decoded_data != None):
+						if (cls.cli == True):
+							print(decoded_data)
+						if (debug == True):
+							print('\nAdding data...\n')
+						cls._replace(cls.html, decoded_data, marker, debug)
+					
+					# Output result
+					if (cls.cli == True):
+						print('\nConverted:\n', cls.html, '\n')
+					return cls.html
+				
+				# Return debug info on error
+				else:
+					print('Failed to parse tags/data.')
+					if (debug == True):
+						print(dir(decoded_tags))
+						print(dir(data))
+					return False
+			else:
+				print('Tags must be a string. ' + type(tags).__name__.capitalize() + ' given.')
+				return False
+		else:
+			print('Empty tags given.')
+			return False
+
+# Required execution code
+def main():
+	# Test payload / data
+	# fail result
+	# payload = ['x', 'y']
+	
+	# pass result
+	# payload = '{"<>":"div","class":"card", "id":"8465416541651365132","html":[{"<>":"img", "src":"https://picsum.photos/id/82/400?random=54654654654","alt":"this is our logo"},{"<>":"p","text":"Hi {{name}}! Welcome to json2html!"}]}'
+	# payload = '{"<>":"div","class":"{{class}}", "id":"8465416541651365132","html":[{"<>":"img", "src":"https://picsum.photos/id/82/400?random=54654654654","alt":"this is our logo"},{"<>":"p","text":"Hi {{name}}! Welcome to json2html!"}]}'
+	# payload = '{"<>":"div","class":"${class}", "id":"8465416541651365132","html":[{"<>":"img", "src":"https://picsum.photos/id/82/400?random=54654654654","alt":"this is our logo"},{"<>":"p","text":"Hi ${name}! Welcome to json2html!"}]}'
+	payload = '{"<>":"div","class":"${class}", "id":"${id}","html":[{"<>":"img", "src":"${src}","alt":"${alt}"},{"<>":"p","text":"Hi ${name}! Welcome to json2html!"}]}'
+
+	# Test data
+	# fail result
+	# data = ''
+	# data = '[]'
+
+	# pass result
+	# data = None
+	# data = '[{"name":"Jo"}]'
+	# data = '[{"id":"element_id"},{"name":"Jo"}]'
+	# data = '[{"class":"card"},{"name":"Jo"}]'
+	data = '[{"class":"card"},{"id":"element_id"},{"src":"https://picsum.photos/id/82/400?random=54654654654"},{"alt":"this is our logo"},{"name":"Jo"}]'
+
+	global json2html
+	# json2html.transform(payload, data, True)
+	json2html.transform(payload, data)
+
+try:
+	main()
+except KeyboardInterrupt:
+	print('\nKeyboardInterrupt has been caught.')
+	exit(1)
